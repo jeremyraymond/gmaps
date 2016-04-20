5efc76c2746499c200fff44f0908632165f116b9
Jeremy Raymond <jeremy.r.raymond@gmail.com>
2016-04-20 14:44:26 -0400

    Removed all of the javascript from this blade file and made it just a div with the appropriate ID, class, and all of the data that the javascript needs to generate a google map inside it.

--- src/gmaps/resources/views/gmaps.blade.php
+++ src/gmaps/resources/views/gmaps.blade.php
@@ -1,37 +1,3 @@
-<script src="https://maps.googleapis.com/maps/api/js?{{ $config['key'] }}"></script>
-<script>
-    function initialize() {
-        // For creating the map object
-        var latlng1 = {lat: {{ $lat1 }}, long: {{ $long1 }}};
-        var latlng2 = {lat: {{ $lat2 }}, long: {{ $long2 }}};
-        // For creating Markers
-        var latlngobj1 = new google.maps.LatLng(latlng1.lat, latlng1.long);
-        var latlngobj2 = new google.maps.LatLng(latlng2.lat, latlng2.long);
+<div id="{{ $config['base_id'] }}{{ $map_num }}" class="gmap" data-base_id="{{ $config['base_id'] }}" data-map_num="{{ $map_num }}" data-config-key="{{ $config['key'] }}" data-config-home_marker="{{ $config['home_marker'] }}" data-config-dest_marker="{{ $config['dest_marker'] }}" data-latlng-lat1="{{ $latlng['lat1'] }}" data-latlng-lng1="{{ $latlng['lng1'] }}" data-latlng-lat2="{{ $latlng['lat2'] }}" data-latlng-lng2="{{ $latlng['lng2'] }}">
 
-        // For creating the map
-        var mapProp = {
-        center:new google.maps.LatLng(latlng1.lat, latlng1.long),
-        zoom:5,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
-        };
-        var map=new google.maps.Map(document.getElementById("map"),mapProp);
-
-        // Create the Markers
-        var marker1=new google.maps.Marker({
-        position:latlngobj1,
-        map: map
-        });
-        var marker2=new google.maps.Marker({
-        position:latlngobj2,
-        map: map
-        });
-
-        // Set the bounds and auto zoom
-        var latlngbounds = new google.maps.LatLngBounds();
-        latlngbounds.extend(latlngobj1);
-        latlngbounds.extend(latlngobj2);
-
-        map.fitBounds(latlngbounds);
-    }
-    google.maps.event.addDomListener(window, 'load', initialize);
-</script>
+</div>
