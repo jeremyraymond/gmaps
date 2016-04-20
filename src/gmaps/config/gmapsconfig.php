92c3bcda2ecf9e7eb5b9dc84df60d0ed3ec3576a
Jeremy Raymond <jeremy.r.raymond@gmail.com>
2016-04-20 14:40:45 -0400

    Added base_id so the id could be customized by the user if they'd like.

--- src/gmaps/config/gmapsconfig.php
+++ src/gmaps/config/gmapsconfig.php
@@ -2,8 +2,9 @@
 
 return array(
 
-    'key' => env('GOOGLE_API_KEY', 'AIzaSyAtqWsq5Ai3GYv6dSa6311tZiYKlbYT4mw'),
-    'home_marker' => env('HOME_MARKER', ''),
-    'dest_marker' => env('DEST_MARKER', '')
+    'key'           => env('GOOGLE_API_KEY', 'AIzaSyAtqWsq5Ai3GYv6dSa6311tZiYKlbYT4mw'), // API key that gets tagged onto the script url
+    'base_id'       => env('BASE_ID', 'map'),  // The base id for the container div that the map number gets tagged onto
+    'home_marker'   => env('HOME_MARKER', ''), // The marker used for the origin latlng
+    'dest_marker'   => env('DEST_MARKER', '')  // The marker used for the destination latlng
 
 );
