c5408043f85436ffab0db44f7cd1ab18beab5947
Jeremy Raymond <jeremy.r.raymond@gmail.com>
2016-04-22 12:48:59 -0400

    Added setOrigin and setDestination functions to simplify setting the latitude and longitude rather than passing a complex multidimensional array to render().

--- src/gmaps/Gmaps.php
+++ src/gmaps/Gmaps.php
@@ -7,16 +7,30 @@ use View;
 class Gmaps
 {
     private $data = [];
+    private $latlng1 = [];
+    private $latlng2 = [];
     
     public function __construct($config) {
         // Get config data and assign it to $data
         $this->data['config'] = $config;
+        $this->latlng1 = ['lat1' => '', 'lng1' => ''];
+        $this->latlng2 = ['lat2' => '', 'lng2' => ''];
+    }
+
+    public function setOrigin($lat, $lng) {
+        $this->latlng1['lat1'] = $lat;
+        $this->latlng1['lng1'] = $lng;
+    }
+
+    public function setDestination($lat, $lng) {
+        $this->latlng2['lat2'] = $lat;
+        $this->latlng2['lng2'] = $lng;
     }
 
     // Return the html rendered from gmaps.blade.php with the $data array
-    public function render($latlng, $map_num) {
-
-        $this->data['latlng'] = $latlng;
+    public function render($map_num = 1) {
+        
+        $this->data['latlng'] = array_merge($this->latlng1, $this->latlng2);
         $this->data['map_num'] = $map_num;
 
         return View::make('gmaps::gmaps')->with($this->data)->render();
