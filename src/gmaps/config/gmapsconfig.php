d2cd4e86743f773d376cef0090d7b1754505064b
Jeremy Raymond <jeremy.r.raymond@gmail.com>
2016-04-22 12:49:17 -0400

    Added zoom to the config file

--- src/gmaps/config/gmapsconfig.php
+++ src/gmaps/config/gmapsconfig.php
@@ -4,6 +4,7 @@ return array(
 
     'key'           => env('GOOGLE_API_KEY', 'AIzaSyAtqWsq5Ai3GYv6dSa6311tZiYKlbYT4mw'), // API key that gets tagged onto the script url
     'base_id'       => env('BASE_ID', 'map'),  // The base id for the container div that the map number gets tagged onto
+    'zoom'          => env('ZOOM', '7'),       // The zoom level for when it just displays one marker
     'home_marker'   => env('HOME_MARKER', ''), // The marker used for the origin latlng
     'dest_marker'   => env('DEST_MARKER', '')  // The marker used for the destination latlng
 
